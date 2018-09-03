44b37e7879e7d364c1272fcc27e82cfa46c8a338
wuwenmin <wuwenmin@diynova.com>
2018-09-03 09:57:17 +0800

    for ticket #54

--- explorer/explorer/settings_local.py
+++ explorer/explorer/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = True
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 55
+STATIC_DEFAULT_VERSION = 56
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
