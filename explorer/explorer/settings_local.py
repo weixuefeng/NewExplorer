be0989bd80cfa23eee121101ec9e26f9970e78b3
tiankai <7871735978@qq.com>
2019-07-03 22:01:54 +0800

    adjust style

--- explorer/explorer/settings_local.py
+++ explorer/explorer/settings_local.py
@@ -13,7 +13,7 @@ APPEND_SLASH = False
 
 LOGGING_API_REQUEST = True
 
-STATIC_DEFAULT_VERSION = 92
+STATIC_DEFAULT_VERSION = 93
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
